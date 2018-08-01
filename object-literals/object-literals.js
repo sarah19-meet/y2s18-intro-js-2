49f2f139567bb709db6cae1e02f1aa2e1da0a254
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 12:14:19 +0300

    sukdtdks

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,11 @@
 // Create your 'me' object literal here!
+var Sarah= {
+	firstName:"Sarah",
+	lastName:"Alayan",
+	favoriteFoods:["pizza","pasta","fries"],
+	age:16,
+	picture:"https://www.google.co.il/search?hl=iw&biw=1299&bih=647&tbm=isch&sa=1&ei=unFhW6fBN4mzkwX1n4iIBQ&q=cute+dogs&oq=cute+dogs&gs_l=img.3..0i67k1j0l2j0i7i30k1l4j0i30k1l3.18108.20557.0.20761.6.6.0.0.0.0.131.502.0j4.4.0....0...1c.1.64.img..2.4.498...0i10i19k1j0i7i30i19k1.0.68_7TyUGxso#imgdii=SyyL3GaUV5Lw9M:&imgrc=N8PrMd2Z5hfmfM:"
+
+}
+
+document.write(JSON.stringify(Sarah));
