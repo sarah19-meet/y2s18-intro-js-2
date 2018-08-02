fa2c2f573e17a954ccc9d9f8b330a3c0645cd29f
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 17:22:58 +0300

    skuhcda

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -2,12 +2,19 @@
 var pictures = ['./imgs/dog.jpg'];
 var currentIndex = 0;
 
-function showNextPicture() {
-  currentIndex++; // increment current picture
-  // if currentIndex is too large, start from the beginning again
-  if (currentIndex >= pictures.length) {
-    currentIndex = 0;
-  }
+// function showNextPicture() {
+//   currentIndex++; // increment current picture
+//   // if currentIndex is too large, start from the beginning again
+//   if (currentIndex >= pictures.length) {
+//     currentIndex = 0;
+//   }
+
+var pic=document.getElementsByTagName('img')[0];
+pic.addEventListener("click",function(event){
+alert("you clicked!");
+});
+
+
 
   // YOUR TASK: Finish this function!
-}
+// }
